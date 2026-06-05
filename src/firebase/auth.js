69d89387257bc261f43ae22e3fd75ba14ae533f7
suryajani283-droid <suryajani283@gmail.com>
2026-06-05 11:45:53 +0530

    Update auth.js

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -4,6 +4,8 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   signOut,
+  signInWithPhoneNumber,
+  RecaptchaVerifier,
 } from 'firebase/auth';
 import { auth } from './config';
 
@@ -18,4 +20,27 @@ export const registerWithEmail = (name, email, password) =>
 export const loginWithGoogle = () =>
   signInWithPopup(auth, googleProvider);
 
-export const logoutUser = () => signOut(auth); 
+export const logoutUser = () => signOut(auth);
+
+// ---- Phone Auth Functions ----
+
+let recaptchaVerifier = null;
+
+export const setupRecaptcha = () => {
+  if (!recaptchaVerifier) {
+    recaptchaVerifier = new RecaptchaVerifier(auth, 'recaptcha-container', {
+      size: 'invisible',
+      callback: () => {
+        // reCAPTCHA solved, allow signInWithPhoneNumber.
+      },
+    });
+  }
+};
+
+export const loginWithPhone = (phoneNumber) => {
+  setupRecaptcha(); // ensure verifier is ready
+  return signInWithPhoneNumber(auth, phoneNumber, recaptchaVerifier);
+};
+
+export const verifyPhoneOTP = (confirmationResult, otp) =>
+  confirmationResult.confirm(otp);

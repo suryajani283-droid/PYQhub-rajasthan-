fe35998cf8deff67c7dca6bfb99bfde109203638
suryajani283-droid <suryajani283@gmail.com>
2026-06-05 09:19:59 +0530

    Update auth.js

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,1 +1,21 @@
- 
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+} from 'firebase/auth';
+import { auth } from './config';
+
+const googleProvider = new GoogleAuthProvider();
+
+export const loginWithEmail = (email, password) =>
+  signInWithEmailAndPassword(auth, email, password);
+
+export const registerWithEmail = (name, email, password) =>
+  createUserWithEmailAndPassword(auth, email, password);
+
+export const loginWithGoogle = () =>
+  signInWithPopup(auth, googleProvider);
+
+export const logoutUser = () => signOut(auth); 

11d145bc49c4cc0214a2a09cfee35d08ecdef19e
suryajani283-droid <suryajani283@gmail.com>
2026-06-05 09:27:59 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,1 +1,20 @@
- 
+import { Helmet } from 'react-helmet-async';
+import HeroBanner from '../components/home/HeroBanner';
+import ExamCategories from '../components/home/ExamCategories';
+import PremiumBundles from '../components/home/PremiumBundles';
+import FreePapers from '../components/home/FreePapers';
+
+export default function HomePage() {
+  return (
+    <>
+      <Helmet>
+        <title>Rajasthan Competition PYQ | PYQHub Rajasthan</title>
+        <meta name="description" content="RAS, REET, LDC, Patwari, Police previous year papers PDF download." />
+      </Helmet>
+      <HeroBanner />
+      <ExamCategories />
+      <PremiumBundles />
+      <FreePapers />
+    </>
+  );
+} 

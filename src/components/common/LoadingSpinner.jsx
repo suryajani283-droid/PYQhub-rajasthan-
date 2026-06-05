d0817e4b5b2e9a1b342d0dccafe31a229852033a
suryajani283-droid <suryajani283@gmail.com>
2026-06-05 09:23:46 +0530

    Update LoadingSpinner.jsx

--- src/components/common/LoadingSpinner.jsx
+++ src/components/common/LoadingSpinner.jsx
@@ -1,1 +1,7 @@
- 
+export default function LoadingSpinner() {
+  return (
+    <div className="flex justify-center items-center py-10">
+      <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-blue-600"></div>
+    </div>
+  );
+}

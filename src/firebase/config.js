42c4af869498c1a04d01ba9026de29d69acd1bb8
suryajani283-droid <suryajani283@gmail.com>
2026-06-05 09:45:46 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,6 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD1R1dOYjhr9SY4saZihXr5YpIFWr0Dut8",
   authDomain: "pyqhub-rajasthan.firebaseapp.com",
@@ -21,4 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app); 
+export const storage = getStorage(app);

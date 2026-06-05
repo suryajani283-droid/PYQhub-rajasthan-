5f55bba2d40eef2448dedc76fac549145632dc4b
suryajani283-droid <suryajani283@gmail.com>
2026-06-05 09:20:29 +0530

    Update storage.js

--- src/firebase/storage.js
+++ src/firebase/storage.js
@@ -1,1 +1,8 @@
- 
+import { storage } from './config';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+
+export const uploadPDF = async (file, path) => {
+  const storageRef = ref(storage, path);
+  await uploadBytes(storageRef, file);
+  return await getDownloadURL(storageRef);
+};
